3188b47bf04422f06abb5bc2bb90135bac09706a
IgnasStrel <ignaslab46@gmail.com>
2025-02-03 20:59:55 +0200

    .

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,7 +1,7 @@
 // src/components/LandingPage.js
 import React from 'react';
 import { useNavigate } from 'react-router-dom'; // Import useNavigate
-import './LandingPage.css';
+import './LandingPage.css'; // Make sure to import your CSS file
 
 const LandingPage = () => {
     const navigate = useNavigate(); // Get the navigate function
@@ -14,9 +14,15 @@ const LandingPage = () => {
         <div className="landing-page">
             <h1 className="app-name">MakeM</h1>
             <div className="options">
-                <button className="option-button" onClick={() => handleNavigation('/make-a-song')}>Make a Song</button>
-                <button className="option-button" onClick={() => handleNavigation('/tests')}>Tests</button>
-                <button className="option-button" onClick={() => handleNavigation('/learning')}>Learning</button>
+                <button className="option-button" onClick={() => handleNavigation('/make-a-song')}>
+                    Make a Song
+                </button>
+                <button className="option-button" onClick={() => handleNavigation('/tests')}>
+                    Tests
+                </button>
+                <button className="option-button" onClick={() => handleNavigation('/learning')}>
+                    Learning
+                </button>
             </div>
         </div>
     );
